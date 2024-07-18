ddf683a404680939dc1dca0bba77891e31b82f46
Erizeez <zhangyongcheng@sjtu.edu.cn>
2024-07-18 06:51:50 +0000

    ospf_rate_limit_test

--- ospf_test_limits.sh
+++ ospf_test_limits.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #echo "Source, Destination, Test Time, Parallel Streams, Bandwidth(Gbps), CPU Usage(%), Memory Usage(MB)" > $LOGFILE
-LOGFILE="ospf/throughput/throughput_60G.csv"
+LOGFILE="ospf/throughput/throughput_rate30g.csv"
 # DOCKER_STATS_LOGFILE="ospf/resource/1resource.csv"
 # # sudo docker exec -d ospf_perf3 bash -c "> /src/ospf/resource/frr_ospf_resource_1to3_record.csv"
 # # sudo docker exec -d ospf_perf1 bash -c "> /src/ospf/resource/frr_ospf_resource_1to3_1record.csv"
@@ -14,17 +14,27 @@ pid1=$(sudo docker inspect -f '{{.State.Pid}}' ospf_1)
 pid2=$(sudo docker inspect -f '{{.State.Pid}}' ospf_2)
 pid3=$(sudo docker inspect -f '{{.State.Pid}}' ospf_3)
 
-sudo ip netns exec $pid1 tc qdisc del root dev veth1
 sudo ip netns exec $pid1 tc qdisc add root dev veth1 handle 1: htb default 2
-sudo ip netns exec $pid1 tc class add dev veth1 parent 1: classid 1:1 htb rate 50Gbit ceil 50Gbit prio 0
-sudo ip netns exec $pid1 tc class add dev veth1 parent 1:1 classid 1:2 htb rate 50Gbit ceil 50Gbit prio 1 burst 96kbit
-sudo ip netns exec $pid1 tc class add dev veth1 parent 1:1 classid 1:3 htb rate 40Gbit ceil 50Gbit prio 1 burst 96kbit
-sudo ip netns exec $pid1 tc filter add dev veth1 protocol ip parent 1:0 prio 1 u32 match ip src 192.168.2.5/24 flowid 1:2
+sudo ip netns exec $pid1 tc class add dev veth1 parent 1: classid 1:1 htb rate 30Gbit ceil 30Gbit prio 0
+sudo ip netns exec $pid1 tc class add dev veth1 parent 1:1 classid 1:2 htb rate 30Gbit ceil 30Gbit prio 1 burst 96kbit
+sudo ip netns exec $pid1 tc qdisc add dev veth1 parent 1:2 handle 10: sfq perturb 10
 
+sudo ip netns exec $pid2 tc qdisc add root dev veth2 handle 1: htb default 2
+sudo ip netns exec $pid2 tc class add dev veth2 parent 1: classid 1:1 htb rate 30Gbit ceil 30Gbit prio 0
+sudo ip netns exec $pid2 tc class add dev veth2 parent 1:1 classid 1:2 htb rate 30Gbit ceil 30Gbit prio 1 burst 96kbit
+sudo ip netns exec $pid2 tc qdisc add dev veth2 parent 1:2 handle 10: sfq perturb 10
 
+sudo ip netns exec $pid2 tc qdisc add root dev veth3 handle 1: htb default 2
+sudo ip netns exec $pid2 tc class add dev veth3 parent 1: classid 1:1 htb rate 30Gbit ceil 30Gbit prio 0
+sudo ip netns exec $pid2 tc class add dev veth3 parent 1:1 classid 1:2 htb rate 30Gbit ceil 30Gbit prio 1 burst 96kbit
+sudo ip netns exec $pid2 tc qdisc add dev veth3 parent 1:2 handle 10: sfq perturb 10
 
+sudo ip netns exec $pid3 tc qdisc add root dev veth4 handle 1: htb default 2
+sudo ip netns exec $pid3 tc class add dev veth4 parent 1: classid 1:1 htb rate 30Gbit ceil 30Gbit prio 0
+sudo ip netns exec $pid3 tc class add dev veth4 parent 1:1 classid 1:2 htb rate 30Gbit ceil 30Gbit prio 1 burst 96kbit
+sudo ip netns exec $pid3 tc qdisc add dev veth4 parent 1:2 handle 10: sfq perturb 10
 
-sudo ip netns exec $pid1 iperf3 -s
+sudo ip netns exec $pid1 iperf3 -s &
 sleep 5
 
 # 运行 docker stats 并将输出重定向到文件
